2b697fb2b3f69378fe9cac531d1876ed18d0764c
gracebir <grace.devolop@gmail.com>
2023-03-16 20:40:20 +0200

    card colors and bg array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,30 @@ import verbal from '../assets/images/icon-verbal.svg'
 import visual from '../assets/images/icon-visual.svg'
 
 const images = [reaction, memory, verbal, visual]
+const colors = [
+    {
+        color: 'text-accent-1',
+        bg: 'bg-b-accent-1'
+    },
+    {
+        color: 'text-accent-2',
+        bg: 'bg-b-accent-2'
+    },
+    {
+        color: 'text-accent-3',
+        bg: 'bg-b-accent-3'
+    },
+    {
+        color: 'text-accent-4',
+        bg: 'bg-b-accent-4'
+    }
+]
 function Card({category,score, index}) {
   return (
-    <div className={`bg-b-accent-${index+1} rounded-lg px-4 py-3 flex justify-between items-center`}>
+    <div className={`${colors[index].bg} rounded-lg px-4 py-3 flex justify-between items-center`}>
       <div className='flex gap-3 items-center'>
         <img src={images[index]} alt={category} />
-        <p className={`text-accent-${index+1} text-[18px]`}>{category}</p>
+        <p className={`${colors[index].color} text-[18px]`}>{category}</p>
       </div>
       <p className='text-clr-lavender'><span className='text-clr-dark-gray font-semibold'>{score}</span> / 100</p>
     </div>
